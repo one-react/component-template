c0c55c1a3e860492c75576b47bf3b050b51d15b5
foryuki <foryuki@outlook.com>
2018-09-18 08:25:35 +0800

    feat: update lint, code styles and release related tools

--- template/src/a.ts
+++ template/src/a.ts
@@ -1,8 +1,12 @@
+export default 'world!'
+
 export class C {
-  x = 10
-  getX = () => this.x;
-  setX = (newVal: number) => { this.x = newVal; }
+  public x = 10
+  public getX = () => this.x
+  public setX = (newVal: number) => {
+    this.x = newVal
+  }
 }
 
 export let x = new C()
-export let y = { ...{ some: "value" } }
+export let y = { ...{ some: 'value' } }
