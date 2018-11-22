3865abc2ce5b942380118c5530cd93306f82471f
foryuki <foryuki@outlook.com>
2018-11-22 20:28:45 +0800

    test: update test

--- tests/main.test.js
+++ tests/main.test.js
@@ -13,13 +13,13 @@ describe('test', () => {
 
     expect(stream.fileList).toMatchSnapshot()
 
-    // const pkg = JSON.parse(await stream.readFile('package.json'))
-    // expect(pkg.name).toBe('or-stub')
-    // expect(pkg.author).toBe('placeholder <placholder@mymail.com>')
-    // expect(pkg.license).toBe('MIT')
+    const pkg = JSON.parse(await stream.readFile('package.json'))
+    expect(pkg.name).toBe('or-stub')
+    expect(pkg.author).toBe('placeholder <placholder@mymail.com>')
+    expect(pkg.license).toBe('MIT')
 
-    // expect(await stream.readFile('readme.md')).toMatchSnapshot('readme.md content')
-    // expect(await stream.readFile('.babelrc.js')).toMatchSnapshot('.babelrc.js')
-    // expect(await stream.readFile('tsconfig.json')).toMatchSnapshot('tsconfig.json')
+    expect(await stream.readFile('readme.md')).toMatchSnapshot('readme.md content')
+    expect(await stream.readFile('.babelrc.js')).toMatchSnapshot('.babelrc.js')
+    expect(await stream.readFile('tsconfig.json')).toMatchSnapshot('tsconfig.json')
   })
 })
